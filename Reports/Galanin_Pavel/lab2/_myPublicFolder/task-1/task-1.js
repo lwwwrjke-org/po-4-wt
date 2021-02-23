f1c3f027b8a0da0460839e93184e260e96682019
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2021-02-23 18:15:37 +0300

    lab2/Galanin_Pavel: fix task 1 with NaN

--- Reports/Galanin_Pavel/lab2/_myPublicFolder/task-1/task-1.js
+++ Reports/Galanin_Pavel/lab2/_myPublicFolder/task-1/task-1.js
@@ -1,47 +1,44 @@
-function isNumber(x) {
-    if (x.replace(/\d/g, '').length) {
-        return 1;
-    }
-    else {
-        return 0;
-    }
-}
-
 while (1) {
     // input a
     let a = prompt("First number\na := ", 10);
-    if (isNumber(a)) {
+    if (isNaN(a)) {
         alert("First input - not a number\n(первый ввод - не число)");
         break;
+    }
+    else {
+        a = parseInt(a);
     }
 
     //input b
     let b = prompt("Second number\nb := ", 12);
-    if (isNumber(b)) {
+    if (isNaN(b)) {
         alert("Second input - not a number\n(второй ввод - не число)");
         break;
     }
-
-    //task
-    if (a == b) {
-        alert(
-            "The numbers are equal\n"
-            + "(числа равны)\n"
-            + `${a} = ${b}`
-        );
+    else {
+        b = parseInt(b);
     }
-    else if (a < b) {
+
+    //task   
+    if (a < b) {
         alert(
             "The first number is less\n"
             + "(первое число меньше)\n"
             + `${a} < ${b}`
         );
     }
-    else {
+    else if (a > b) {
         alert(
             "Second number less\n"
             + "(второе число меньше)\n"
             + `${a} > ${b}`
+        );
+    }
+    else {
+        alert(
+            "The numbers are equal\n"
+            + "(числа равны)\n"
+            + `${a} = ${b}`
         );
     } //end task if
 } //end infinity while
