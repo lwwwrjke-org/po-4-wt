b0fd555fbc5479fd4dcc4179849bf4d7ee60ae28
unknown <gribovskij.01@gmail.com>
2021-03-15 09:25:49 +0300

    lab2/Gribovskij_Dima : fix tabs

--- Reports/Gribovskij_Dima/lab2/task4.js
+++ Reports/Gribovskij_Dima/lab2/task4.js
@@ -1,7 +1,7 @@
 function task4() {
-    while(true) {
+    while (true) {
         const YEAR = 2016
-        let month  = prompt('Enter number of month(1-12)');
+        let month = prompt('Enter number of month(1-12)');
         let day;
         month = Number(month);
         if (month < 1 || month > 12) {
@@ -10,29 +10,29 @@ function task4() {
         }
 
         if (month == 2) {
-          day = prompt('Enter day of the month(1-29)');
-          day = Number(day);
-          if (month == 2 && day > 29) {
-              alert("Invalid input!");
-              continue;
-          }
-        } else {
-          day = prompt('Enter day of the month(1-31)');
-          day = Number(day);
-          if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8
-            || month == 10 || month == 12) {
-            if (day < 1 || day > 31) {
+            day = prompt('Enter day of the month(1-29)');
+            day = Number(day);
+            if (month == 2 && day > 29) {
                 alert("Invalid input!");
                 continue;
             }
-          } else {
-            if (day < 1 || day > 30) {
-              alert("Invalid input!");
-              continue;
+        } else {
+            day = prompt('Enter day of the month(1-31)');
+            day = Number(day);
+            if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8
+                || month == 10 || month == 12) {
+                if (day < 1 || day > 31) {
+                    alert("Invalid input!");
+                    continue;
+                }
+            } else {
+                if (day < 1 || day > 30) {
+                    alert("Invalid input!");
+                    continue;
+                }
             }
-          }
         }
-        
+
         if ((month || day) === null) {
             alert("Invalid input!");
             continue;
@@ -41,29 +41,29 @@ function task4() {
         let date = new Date(YEAR, month - 1, day);
 
         switch (date.getDay()) {
-          case 0:
-            alert('Sunday');
-            break;
-          case 1:
-            alert('Monday');
-            break;
-          case 2:
-            alert('Tuesday');
-            break;
-          case 3:
-            alert('Wednesday');
-            break;
-          case 4:
-            alert('Thursday');
-            break;
-          case 5:
-            alert('Friday');
-            break;
-          case 6:
-            alert('Saturday');
-            break;
-          default:
-            alert('Invalid date!');
+            case 0:
+                alert('Sunday');
+                break;
+            case 1:
+                alert('Monday');
+                break;
+            case 2:
+                alert('Tuesday');
+                break;
+            case 3:
+                alert('Wednesday');
+                break;
+            case 4:
+                alert('Thursday');
+                break;
+            case 5:
+                alert('Friday');
+                break;
+            case 6:
+                alert('Saturday');
+                break;
+            default:
+                alert('Invalid date!');
         }
-    }   
+    }
 }
