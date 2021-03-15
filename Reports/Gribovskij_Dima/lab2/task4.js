dec6e81f98a150537a48edbf03e6592614bf64b5
unknown <gribovskij.01@gmail.com>
2021-03-15 09:10:05 +0300

    lab2/Gribovskij_Dima: fix task4.js and index.html

--- Reports/Gribovskij_Dima/lab2/task4.js
+++ Reports/Gribovskij_Dima/lab2/task4.js
@@ -10,19 +10,27 @@ function task4() {
         }
 
         if (month == 2) {
-            day = prompt('Enter day of the month(1-29)');
-            day = Number(day);
-            if (month == 2 && day > 29) {
-                alert("Invalid input!");
-                continue;
-            }
+          day = prompt('Enter day of the month(1-29)');
+          day = Number(day);
+          if (month == 2 && day > 29) {
+              alert("Invalid input!");
+              continue;
+          }
         } else {
-            day = prompt('Enter day of the month(1-31)');
-            day = Number(day);
+          day = prompt('Enter day of the month(1-31)');
+          day = Number(day);
+          if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8
+            || month == 10 || month == 12) {
             if (day < 1 || day > 31) {
                 alert("Invalid input!");
                 continue;
             }
+          } else {
+            if (day < 1 || day > 30) {
+              alert("Invalid input!");
+              continue;
+            }
+          }
         }
         
         if ((month || day) === null) {
