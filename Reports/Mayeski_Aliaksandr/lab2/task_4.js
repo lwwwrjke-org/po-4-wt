beb4deb163e6657986005b8e0c50de3f5643496c
GroD-off <54220362+GroD-off@users.noreply.github.com>
2021-03-16 17:25:34 +0300

    Update task_4.js

--- Reports/Mayeski_Aliaksandr/lab2/task_4.js
+++ Reports/Mayeski_Aliaksandr/lab2/task_4.js
@@ -1,4 +1,31 @@
 function TASK_4() {
+    var day = prompt("Введите день")
+    var month = prompt("Введите месяц")
+    var year = prompt("Введите год")
+    day = Number.parseInt(day)
+    month = Number.parseInt(month)
+    year = Number.parseInt(year)
+    if (year % 4 == 0 && year % 100 != 0)
+    {
+        if (month == 2) {
+            if (day >= 1 && day <= 29) { }
+            else {
+                alert("Error")
+                return 1
+            }
+
+        }
+    }
+    else {
+        if (month == 2) {
+            if (day >= 1 && day <= 28) { }
+            else {
+                alert("Error")
+            }
+
+        }
+        return 1
+    }
     var days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
     var date = new Date(prompt('введите дату', ''));
     alert(days[date.getDay()])
