13a176df488f6a54398d7d3c0449cce368300c31
LudAlexey <aleslyd5@gmail.com>
2021-03-16 23:08:34 +0300

    lab3/Lud_Alexey : Make lab3

---
+++ Reports/Lud_Alexey/lab3/3lab(1).js
@@ -0,0 +1,26 @@
+while(1){
+  
+function range(start, end, step) {
+  if (step == null) step = 1;
+  var range = [];
+  if (step > 0) {
+    for (let i = start; i <= end; i += step)
+      range.push(i);
+  } else {
+    for (let i = start; i >= end; i += step)
+      range.push(i);
+  }
+  return range;
+}
+
+var start = prompt("Введите начало: "); var st = Number(start);
+if(isNaN(start)){ alert("Неверные данные"); throw "Error"; }
+
+var end = prompt("Введите конец: "); var e = Number(end);
+if(isNaN(end)){ alert("Неверные данные"); throw "Error"; }
+
+var step = prompt("Введите шаг: "); var s = Number(step);
+if(isNaN(step)){ alert("Неверные данные"); throw "Error"; }
+
+alert("range(" + st + "," + e + "," + s + ")= " + "[" + range(st, e, s) + "]");
+}
