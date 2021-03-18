d6c2123a794f97ecfc7bc796b34d55f29e5b924e
Anastasiya Vorobey <72712452+DoctorKocte@users.noreply.github.com>
2021-03-18 13:06:20 +0300

    Update task-3.js

--- Reports/Vorobey_Anastasiya/lab2/task-3.js
+++ Reports/Vorobey_Anastasiya/lab2/task-3.js
@@ -1,6 +1,10 @@
     let button = document.querySelector('.button');
     let fib = (n) => {
       let a = 0, b = 1;
+      if (n == 0) {
+            alert("0, но программа считает числа с единицы. То есть при вводе 1 вновь будет выведен 0.")
+            ExtensionScriptApis;
+      }
       if (n == 1) { //подсчет с 0
         b = 0;
       } else {
