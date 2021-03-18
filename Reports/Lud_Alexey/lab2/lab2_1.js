bc73e2749bd509be5d191d7d52084c1817170335
LudAlexey <aleslyd5@gmail.com>
2021-03-18 18:43:29 +0300

    lasb2/Lud_Alexey : fixed lab2(sorry, i was wrong)

--- Reports/Lud_Alexey/lab2/lab2_1.js
+++ Reports/Lud_Alexey/lab2/lab2_1.js
@@ -5,13 +5,13 @@ function lab2_1(){
         alert("Первый ввод - не число");
         break;
     }
+    n1 = Number(num1);
     var num2 = prompt("Введите второе число:");
     if (isNaN(num2)){
         alert("Второй ввод - не число");
         break;
     }
-    n1 = Number.parseInt(num1);
-    n2 = Number.parseInt(num2);
+    n2 = Number(num2);
     if (n1 == n2){
         alert("Числа равны");
     } else if (n1 > n2){
