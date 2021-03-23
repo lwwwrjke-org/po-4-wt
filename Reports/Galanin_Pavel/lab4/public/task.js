7497470bd5de95e3e24f691327bd67077da7553b
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2021-03-24 01:33:58 +0300

    lab4/Galanin_Pavel : Deleted class. Added function

--- Reports/Galanin_Pavel/lab4/public/task.js
+++ Reports/Galanin_Pavel/lab4/public/task.js
@@ -1,180 +1,150 @@
-class lab4 {
-    constructor(GH_login = "", GH_token = "") {
-        console.log("Constructor lab4 run");
-        this.GH_login = GH_login;
-        this.GH_token = GH_token;
-        if (this.GH_token != '') {
-            this.headers = {
-                "Authorization": `Token ${this.GH_token}`
+function lab4(GH_login = "", GH_token = "") {
+    let url = `https://api.github.com/users/${GH_login}`;
+    fetch(url)
+        .then(response => response.json())
+        .then((data) => {
+            if (data.message) {
+                alert(data.message);
             }
-        }
-    }
-    get() {
-        let url = `https://api.github.com/users/${this.GH_login}`;
-        console.log(`url = ${url}`);
-        fetch(url, {
-            "method": "GET",
-            "headers": this.headers
+            return data;
         })
-            // JSON to JS object
-            .then(response => response.json())
-            // Save JS object
-            .then((data) => {
-                if (data.message) {
-                    alert(data.message);
-                }
+        .then((data) => {
+            // b-profile
+            let avatar_url_html_id = 'GH__avatar_url';
+            if (document.getElementById(avatar_url_html_id)) {
+                document.getElementById(avatar_url_html_id).innerHTML
+                    = `<img src="${data['avatar_url']}" alt="" />`;
+            } else {
+                console.error(`Not found id="${avatar_url_html_id}"`);
+            }
 
-                this.data = data;
-                console.log(`this.data = ${this.data} =`);
-                console.log(this.data);
-            })
-            .then(() => {
-                // b-profile
-                let avatar_url_html_class = 'GH__avatar_url';
-                if (document.getElementById(avatar_url_html_class)) {
-                    document.getElementById(avatar_url_html_class).innerHTML
-                        = `<img src="${this.data['avatar_url']}" alt="" />`;
-                } else {
-                    console.error(`Not found id="${avatar_url_html_class}"`);
-                }
+            let name_html_id = 'GH__name';
+            if (document.getElementById(name_html_id)) {
+                document.getElementById(name_html_id).innerHTML
+                    = data['name'];
+            } else {
+                console.error(`Not found id="${name_html_id}"`);
+            }
 
-                let name_html_class = 'GH__name';
-                if (document.getElementById(name_html_class)) {
-                    document.getElementById(name_html_class).innerHTML
-                        = this.data['name'];
-                } else {
-                    console.error(`Not found id="${name_html_class}"`);
-                }
+            let login_html_id = 'GH__login';
+            if (document.getElementById(login_html_id)) {
+                document.getElementById(login_html_id).innerHTML
+                    = data['login'];
+            } else {
+                console.error(`Not found id="${login_html_id}"`);
+            }
+            // end b-profile
+            return data;
+        })
+        .then((data) => {
+            // b-stats
+            let followers_html_id = 'GH__followers';
+            if (document.getElementById(followers_html_id) != undefined) {
+                document.getElementById(followers_html_id).innerHTML = data['followers'];
+            } else {
+                console.error(`Not found id="${followers_html_id}"`);
+            }
 
-                let login_html_class = 'GH__login';
-                if (document.getElementById(login_html_class)) {
-                    document.getElementById(login_html_class).innerHTML
-                        = this.data['login'];
-                } else {
-                    console.error(`Not found id="${login_html_class}"`);
-                }
-                // end b-profile
-            })
-            .then(() => {
-                // b-stats
-                let followers_html_id = 'GH__followers';
-                if (document.getElementById(followers_html_id) != undefined) {
-                    document.getElementById(followers_html_id).innerHTML = this.data['followers'];
-                } else {
-                    console.error(`Not found id="${followers_html_id}"`);
-                }
+            let following_html_id = 'GH__following';
+            if (document.getElementById(following_html_id) != undefined) {
+                document.getElementById(following_html_id).innerHTML = data['following'];
+            } else {
+                console.error(`Not found id="${following_html_id}"`);
+            }
+            // end b-stats
+            return data;
+        })
+        // Get info about followers
+        .then((data) => {
+            fetch(data['followers_url'])
+                .then(response => response.json())
+                .then(data => {
+                    if (data.message) {
+                        alert(data.message);
+                    }
 
-                let following_html_id = 'GH__following';
-                if (document.getElementById(following_html_id) != undefined) {
-                    document.getElementById(following_html_id).innerHTML = this.data['following'];
-                } else {
-                    console.error(`Not found id="${following_html_id}"`);
-                }
-                // end b-stats
-            })
-            // Get info about followers
-            .then(() => {
-                fetch(this.data['followers_url'], {
-                    "method": "GET",
-                    "headers": this.headers
+                    let html = '';
+                    for (let i = 0; i < data.length; i++) {
+                        html +=
+                            `<div class="b-followers__b-follower">`
+                            + `<div class="b-followers__img">`
+                            + `<img src="${data[i]['avatar_url']}" alt="${data[i]['login']}" />`
+                            + `</div>`
+                            + `<div class="b-followers__info">`
+                            + `<a href="${data[i]['html_url']}">${data[i]['login']}</a>`
+                            + `</div>`
+                            + `</div>`
+                            + `</div>`;
+                    }
+
+                    let followers_html_id = 'GH__followers_url';
+                    if (document.getElementById(followers_html_id) != undefined) {
+                        document.getElementById(followers_html_id).innerHTML = html;
+                    } else {
+                        console.error(`Not found id="${followers_html_id}"`);
+                    }
+
+                    return data;
                 })
-                    .then(response => response.json())
-                    .then(data => {
-                        if (data.message) {
-                            alert(data.message);
-                        }
+            return data;
+        })
+        //Get info about repositories
+        .then((data) => {
+            let repos_url = data.repos_url;
+            fetch(repos_url)
+                .then(response => response.json())
+                .then(data => {
+                    if (data.message) {
+                        alert(data.message);
+                    }
 
-                        let html = '';
-                        for (let i = 0; i < data.length; i++) {
-                            html +=
-                                `<div class="b-followers__b-follower">`
-                                + `<div class="b-followers__img">`
-                                + `<img src="${data[i]['avatar_url']}" alt="${data[i]['login']}" />`
-                                + `</div>`
-                                + `<div class="b-followers__info">`
-                                + `<a href="${data[i]['html_url']}">${data[i]['login']}</a>`
-                                + `</div>`
-                                + `</div>`
-                                + `</div>`;
-                        }
+                    let html = '';
+                    for (let i = 0; i < data.length; i++) {
+                        html +=
+                            `<div class="b-repos__repository">`
+                            + `<div class="b-repos__name">`
+                            + `<a href="${data[i]['html_url']}">${data[i]['name']}</a>`
+                            + `</div>`
+                            + `<div class="b-repos__description">`
+                            + `${data[i]['description'] == null ? '' : data[i]['description']}`
+                            + `</div>`
+                            + `</div>`;
+                    }
 
-                        let followers_html_id = 'GH__followers_url';
-                        if (document.getElementById(followers_html_id) != undefined) {
-                            document.getElementById(followers_html_id).innerHTML = html;
-                        } else {
-                            console.error(`Not found id="${followers_html_id}"`);
-                        }
-                    })
-            })
-            //Get info about repositories
-            .then(() => {
-                let repos_url = this.data.repos_url;
-                console.log(`repos_url = ${repos_url}`);
-                fetch(repos_url, {
-                    "method": "GET",
-                    "headers": this.headers
+                    let repos_html_id = 'GH__repos_url';
+                    if (document.getElementById(repos_html_id) != undefined) {
+                        document.getElementById(repos_html_id).innerHTML = html;
+                    } else {
+                        alert(`Not found id="${repos_html_id}"`);
+                    }
+
+                    return data;
                 })
-                    .then(response => response.json())
-                    .then(data => {
-                        if (data.message) {
-                            alert(data.message);
-                        }
-
-                        let html = '';
-                        for (let i = 0; i < data.length; i++) {
-                            html +=
-                                `<div class="b-repos__repository">`
-                                + `<div class="b-repos__name">`
-                                + `<a href="${data[i]['html_url']}">${data[i]['name']}</a>`
-                                + `</div>`
-                                + `<div class="b-repos__description">`
-                                + `${data[i]['description'] == null ? '' : data[i]['description']}`
-                                + `</div>`
-                                + `</div>`;
-                        }
-
-                        let repos_html_id = 'GH__repos_url';
-                        if (document.getElementById(repos_html_id) != undefined) {
-                            document.getElementById(repos_html_id).innerHTML = html;
-                        } else {
-                            alert(`Not found id="${repos_html_id}"`);
-                        }
-
-                    })
-            });
-    }
+        });
 }
 
-if (document.getElementById('lab4__button') != undefined) {
-    document.getElementById('lab4__button').onclick = function () {
+let lab4_button_html_id = 'lab4__button';
+if (document.getElementById(lab4_button_html_id) != undefined) {
+    document.getElementById(lab4_button_html_id).onclick = function () {
         //get login
+        let login_html_id = 'lab4__login-input';
         let GH_login = '';
-        if (document.getElementById('lab4__login-input') != undefined) {
-            let login_input = document.getElementById('lab4__login-input');
-
-            GH_login = login_input.value;
-
-            console.log(`GH_login = $ { GH_login }`);
+        if (document.getElementById(login_html_id) != undefined) {
+            GH_login = document.getElementById(login_html_id).value;
         } else {
-            alert(`Not found id="lab4__login-input"`);
+            alert(`Not found id="${login_html_id}"`);
         }
-
-
+        let token_html_id = 'lab4__token-input';
         //get token
         let GH_token = '';
-        if (document.getElementById('lab4__token-input') != undefined) {
-            let token_input = document.getElementById('lab4__token-input');
-            GH_token = token_input.value;
-            console.log(`GH_token = $ { GH_token }`);
+        if (document.getElementById(token_html_id) != undefined) {
+            GH_token = document.getElementById(token_html_id).value;
         } else {
-            alert(`Not found id="lab4__token-input"`)
+            alert(`Not found id="${token_html_id}"`)
         }
-
-
-        //create object
-        let obj = new lab4(GH_login, GH_token);
-        obj.get();
+        //
+        lab4(GH_login, GH_token);
     }
 } else {
-    alert(`Not found id="lab4__button"`);
+    alert(`Not found id="${lab4_button_html_id}"`);
 }
