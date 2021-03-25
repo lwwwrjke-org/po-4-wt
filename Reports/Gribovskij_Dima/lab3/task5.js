f7b3cbf182ed284b8e09b0c4b1bc51126067965a
NeitLC <gribovskij.01@gmail.com>
2021-03-25 21:21:44 +0300

    lab4/Gribovskij_Dima: add lab4

--- Reports/Gribovskij_Dima/lab3/task5.js
+++
@@ -1,8 +0,0 @@
-function task5() {
-    function flatten(array) {
-        let merged = [].concat.apply([], array);
-        return merged;
-    }
-    array = [1, [2], [3, [4]]]
-    alert(flatten(array));
-}
