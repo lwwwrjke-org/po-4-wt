14819b0a05b50e48fa6daa619ee5090a6f38dee8
DanilaSiniak <sinyak2002@gmail.com>
2021-03-30 13:57:12 +0300

    changed zadanie4

--- Reports/Siniak_Danila/lab3/task_4.js
+++ Reports/Siniak_Danila/lab3/task_4.js
@@ -1,19 +1,12 @@
-function TASK_4()
-{
-    function union(arrays) {
-        let a = [],
-            obj = {};
-    
-        for (let i = 0; i < arrays.length; i++) {
-          a = a.concat(arrays[i]);
-        }
-    
-        for (let i = 0; i < a.length; i++) {
-          let item = a[i];
-          obj[item] = true;
-        }
-        return Object.keys(obj);
-      }
-    
-      alert(union([1, 2, 2, 3], [101, 2, 1, 10], [2, 1]) );
-}
+function TASK_4() {
+  function union() {
+      let arrRows = [];
+      arrRows = new Set(arrRows.concat.apply(arrRows, arguments));
+      return [...arrRows];
+  }
+
+  a = [1, 2, 2, 3];
+  b = [101, 2, 1, 10];
+  c = [2, 1];
+  alert(union(a,b,c));
+} 
