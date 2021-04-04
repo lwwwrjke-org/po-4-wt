5d9a45aebc15c07ac12e97b17f004ae3910487d1
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2021-04-04 18:56:24 +0300

    lab5/Galanin_Pavel : Added function get_fetch

--- Reports/Galanin_Pavel/lab5/src/components/App/App.jsx
+++ Reports/Galanin_Pavel/lab5/src/components/App/App.jsx
@@ -1,11 +1,19 @@
 function App() {
     function get_login(event) {
-        console.log(event.target.value);
+        let login = event.target.value;
+        console.log(login);
+        get_fetch(login);
+    }
+
+    function get_fetch(login = '') {
+        fetch(`https://api.github.com/users/${login}`)
+            .then(response => response.json())
+            .then(data => console.log(data));
     }
 
     return (
         <div className="App">
-            <input type="text" placeholder="GitHub login" onInput={get_login} />
+            <input type="text" placeholder="GitHub login" onInput={get_login} value="Pavel-Innokentevich-Galanin" />
         </div>
     );
 }
