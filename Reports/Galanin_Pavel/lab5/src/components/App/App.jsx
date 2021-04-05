9254992ac0e14b6a1b0133317416fd29f2c61e88
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2021-04-06 00:54:16 +0300

    lab5/Galanin_Pavel : Added component repos

--- Reports/Galanin_Pavel/lab5/src/components/App/App.jsx
+++ Reports/Galanin_Pavel/lab5/src/components/App/App.jsx
@@ -22,6 +22,20 @@ class App extends React.Component {
             stars: 0,
             location: "Location",
             name: "Name",
+            repos: [
+                {
+                    name: 'Name 1',
+                    description: 'Description 1',
+                    created_at: null,
+                    updated_at: null,
+                },
+                {
+                    name: 'Name 2',
+                    description: 'Description 2',
+                    created_at: null,
+                    updated_at: null,
+                }
+            ],
         };
     }
 
@@ -92,11 +106,27 @@ class App extends React.Component {
                         </div>
                         <div className={styles.App__b_more_info}>
                             <span className={styles.b_profile__link}>
-                                Created {this['state']['created_at__formated']}
+                                Created {this.get_formated_date(this['state']['created_at'])}
                             </span>
                         </div>
                     </header>
                     {/* end header */}
+                    {/* repos */}
+                    <div>
+                        {
+                            (this['state']['repos']).map(
+                                (obj, index) => (
+                                    <div className={styles.b_repos__repo} key={index}>
+                                        <div>{obj['name']}</div>
+                                        <div>{obj['description']}</div>
+                                        <div>{this.get_formated_date(obj['created_at'])}</div>
+                                        <div>{this.get_formated_date(obj['updated_at'])}</div>
+                                    </div>
+                                )
+                            )
+                        }
+                    </div>
+                    {/* end repos */}
                 </div>
             </div>
         );
@@ -125,6 +155,20 @@ class App extends React.Component {
     }
     // end start_function()
 
+    
+    get_formated_date(param_date)
+    {
+        let obj = new Date(param_date);
+        let year = obj.getFullYear();
+        let month = obj.getMonth() + 1;
+        let date = obj.getDate();
+
+        let hours = obj.getHours();
+        let minutes = obj.getMinutes();
+        let seconds = obj.getSeconds();
+
+        return (<span>{year}.{month}.{date} {hours}:{minutes}:{seconds}</span>);
+    }
 
     fetch_users() {
         fetch(`https://api.github.com/users/${this.state.login}`)
@@ -143,25 +187,6 @@ class App extends React.Component {
                             name: result['name'],
                         }
                     );
-
-                    // get formated created_at
-                    let obj = new Date(this['state']['created_at']);
-
-                    let year = obj.getFullYear();
-                    let month = obj.getMonth() + 1;
-                    let date = obj.getDate();
-
-                    let hours = obj.getHours();
-                    let minutes = obj.getMinutes();
-                    let seconds = obj.getSeconds();
-
-                    this.setState(
-                        {
-                            created_at__formated: `${year}.${month}.${date} ${hours}:${minutes}:${seconds}`,
-                        }
-                    );
-                    // end get formated created_at
-
                     console.log(result);
                 }
             );
