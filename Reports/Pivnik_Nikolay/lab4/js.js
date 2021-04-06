69f647631a3962fea95e62827872e08cd7aa50fb
chelseanvp <chelsea.nvp25@gmail.com>
2021-04-06 11:57:16 +0300

    lab4 Update

--- Reports/Pivnik_Nikolay/lab4/js.js
+++ Reports/Pivnik_Nikolay/lab4/js.js
@@ -6,6 +6,7 @@ form.addEventListener('submit',function(e){
  document.getElementById("location").innerHTML= `<a> </a>`
  document.getElementById("bio").innerHTML= `<a> </a>`
  document.getElementById("fullName").innerHTML= `<a> </a>`
+ clean()
  
  var search = document.getElementById("search").value
  fetch("https://api.github.com/users/"+search)
@@ -55,15 +56,76 @@ form.addEventListener('submit',function(e){
         `<img class ="emailImage" src = "imail.jpg"/> sign in to view email`
 
      document.getElementById("overView").innerHTML = 
-        `<input type="button" value="Overview" class="button-overView" />`
+        `<input type="button" value="Overview" class="button-overView" 
+        onclick = "overViewClick()"/>`
     document.getElementById("repositories").innerHTML = 
-        `<input type="button" value="Repositories" class="button-overView" />`
+        `<input type="button" value="Repositories" class="button-overView"
+        onclick = "overRepositoriesClick()" />`
         document.getElementById("projects").innerHTML = 
-        `<input type="button" value="Projects" class="button-overView" />`
+        `<input type="button" value="Projects" class="button-overView"
+        onclick = "overProjectsClick()"/>`
         document.getElementById("packages").innerHTML = 
-        `<input type="button" value="Packages" class="button-overView" />`  
+        `<input type="button" value="Packages" class="button-overView"
+        onclick = "overPackagesClick()" />`  
         
  })
  
 })
 
+function overViewClick(){
+   document.getElementById("repo").innerHTML += 
+   `<div class="firstColumnWithRepo" id="firstColumnWithRepo"></div>
+    <div class="secondColumnWithRepo" id="secondColumnWithRepo"></div>`
+
+    var search = document.getElementById("search").value
+    fetch("https://api.github.com/users/"+search + "/repos") 
+
+    .then((result) => result.json())
+    .then((data)=> {
+
+      var i = 0;
+      while(data[i] != undefined){
+         if(data[i].language == null){data[i].language = ""}
+         if(i % 2 == 0){
+            document.getElementById("firstColumnWithRepo").innerHTML += 
+            `<div class = "repo" id= "${i}"></div>`
+            document.getElementById(i).innerHTML = 
+            `<a href="${data[i].svn_url}">${data[i].name}</a>
+            <p>${data[i].language} stars ${data[i].stargazers_count}</p>`
+         
+
+         }else if(i % 2 == 1){
+            
+            document.getElementById("secondColumnWithRepo").innerHTML += 
+            `<div class = "repo" id= "${i}" ></div>` 
+            document.getElementById(i).innerHTML = 
+            `<a href="${data[i].svn_url}" >${data[i].name}</a> 
+            <p>${data[i].language} stars ${data[i].stargazers_count}</p>`
+         }
+         i++;
+      }
+
+   })
+
+}
+
+function clean(){
+   document.getElementById("repo").innerHTML = 
+   `<a></a>`
+}
+
+
+function overRepositoriesClick(){
+   document.getElementById("repo").innerHTML = 
+   `<a></a>`
+}
+
+function overProjectsClick(){
+   document.getElementById("repo").innerHTML = 
+   `<a></a>`
+}
+
+function overPackagesClick(){
+   document.getElementById("repo").innerHTML = 
+   `<a></a>`
+}
