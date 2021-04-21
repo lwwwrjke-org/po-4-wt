f8c6562e5e969bb3fcb1c6ca81474cb82b3baa97
MrGodSmile <mrgodsmile@yandex.ru>
2021-04-21 16:31:58 +0300

    lab3/Borovsky_Maksim

--- Reports/3lab/task1.js
+++ Reports/3lab/task1.js
@@ -1,28 +1,13 @@
-function task1() {
-    function range(left, right, step = 1) {
-        let result = [];
-        let defstep = prompt("С шагом по умолчанию??\n1-Да, 2-Нет", 1);
-        if (defstep == 0) {
-            step = parseInt(prompt("Новый шаг\nшаг = ", 1));
-        }
-        if (step > 0) {
-            for (let i = left; i <= right; i += step) {
-                result.push(i);
-            }
-        } else if (step < 0) {
-            for (let i = left; i >= right; i += step) {
-                result.push(i);
-            }
-        } else {
-            alert("Неправильный шаг!");
-        }
-
-        return result;
+function task1() 
+{
+    function range(start, finish, step) {
+        if (typeof step !== 'number') step = 1;
+        if ((start > finish && step > 0) || (start < finish && step < 0) || step === 0) return;
+        var a = [];
+        if (step > 0) for (var i = start; i <= finish; i += step) a.push(i);
+        else for (var i = start; i >= finish; i += step) a.push(i);
+        return a;
     }
 
-    let left  = prompt("Левая граница:\nЛевая = ", 1);
-    left      = parseInt(left);
-    let right = prompt("Правая граница:\nПравая = ", 1);
-    right     = parseInt(right);
-    alert(range(left,right));
+    alert(range(5, 2, -1));
 }
