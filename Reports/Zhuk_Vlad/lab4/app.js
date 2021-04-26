1c713ef92ad0c91fbe8bdbcde9ce2f8003ecc97e
Влад Жук <zva200206@mail.ru>
2021-04-27 01:02:50 +0300

    lab4/Zhuk_Vlad : Made lab4

---
+++ Reports/Zhuk_Vlad/lab4/app.js
@@ -0,0 +1,34 @@
+const github = new Github;  
+
+// Init UI
+const ui = new UI;
+
+// Search Input
+const searchUser = document.getElementById('searchUser');
+
+
+// Search input event listener
+searchUser.addEventListener('keyup', (e) => {
+    // Get input text
+    const userText = e.target.value;
+
+    if(userText !== '') {
+        // Make HTTP call
+        github.getUser(userText)
+        .then(data => { 
+            if(data.profile.message === 'Not Found') {
+                // Show alert
+                ui.showAlert('User Not Found', 'alert alert-danger');
+
+            } else {
+                // Show profile
+                ui.showProfile(data.profile);
+                ui.showRepos(data.repos);
+
+            }
+        })
+    } else {
+        // clear profile
+        ui.clearProfile();
+    }
+}); 
