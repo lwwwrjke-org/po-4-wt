761e9e34515cb64dfb0237ac746d5066c768719e
nastiaShi <forioitukr2017@gmail.com>
2021-05-30 23:06:44 +0300

    lab2/Shiba_Anastasiya: Make lab2

---
+++ Reports/Shiba_Anastasiya/lab2/lab2_3.js
@@ -0,0 +1,11 @@
+function lab2_3(){
+function fib(n) {
+  return n <= 1 ? n : fib(n - 1) + fib(n - 2);
+}
+var i = prompt("Введите параметр i:");
+if(i < 1 || isNaN(i)){
+alert("Неверные данные");
+throw "Error";
+}
+alert("fib("+i+")= "+fib(i));
+}
